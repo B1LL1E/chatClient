e6269dfbdc8c27e840a93496731bfe96d58ddb16
Karol <karol660rata@gmail.com>
2022-11-22 18:51:39 +0100

    zaktualizowano input oraz dodano scrollbar

--- src/strony/Chat.js
+++ src/strony/Chat.js
@@ -26,44 +26,43 @@ export default function Chat() {
 
     return(
         <div id='Chat'>
-            <h2 id='Chat'>CHAT</h2>   
+            <div id='wiadomosci'>
+                <table id='wiadomosci'>           
+                    {list.map((val, key) => {
+                        let idTr = 'tabelaChatP';
+                        let idDiv = 'divP';
+                        let idNazwa = 'nazwaP'
+
+                        if(nick === val.nick){
+                            idTr = 'tabelaChatP'
+                            idDiv = 'divP'
+                            idNazwa = 'nazwaP'
+                        }
+                        else{
+                            idTr = 'tabelaChatL'
+                            idDiv = 'divL'
+                            idNazwa = 'nazwaL'
+                        }
+
+                        return(
+                            <tr id={idTr} key={key}>
+                                <td>
+                                    <div id={idNazwa}>{val.nick}</div>
+                                    <div id={idDiv}>{val.text}</div>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </table>
+            </div>
 
             <div id='nick'>
                 <input id='nick' type='text' value={nick} onChange={(e) => setNick(e.target.value)}/>
             </div>
-            
-
-            <table id='wiadomosci'>
-                {list.map((val, key) => {
-                    let idTr = 'tabelaChatP';
-                    let idDiv = 'divP';
-                    let idNazwa = 'nazwaP'
-
-                    if(nick === val.nick){
-                        idTr = 'tabelaChatP'
-                        idDiv = 'divP'
-                        idNazwa = 'nazwaP'
-                    }
-                    else{
-                        idTr = 'tabelaChatL'
-                        idDiv = 'divL'
-                        idNazwa = 'nazwaL'
-                    }
-
-                    return(
-                        <tr id={idTr} key={key}>
-                            <td>
-                                <div id={idNazwa}>{val.nick}</div>
-                                <div id={idDiv}>{val.text}</div>
-                            </td>
-                        </tr>
-                    )
-                })}
-            </table>
 
             <div id='buttonWyslij'>
-                <input type='text' value={text} onChange={(e) => setText(e.target.value) } id='buttonWyslij' />
-                <button onClick={Wyslij} id='buttonWyslij'>WYSLIJ</button>
+                <input type='text' value={text} onChange={(e) => setText(e.target.value) } id='buttonWyslij' placeholder='Wprowadź Wiadomość' />
+                <button onClick={Wyslij} id='buttonWyslij'>✔</button>
             </div>
         </div>
     )
